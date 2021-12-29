2fca0900890f4e2297567ec1b1d29f83d119df52
MahaGowtham <madhumeena.k1994@gmail.com>
2021-12-29 15:58:52 +0530

    smoke and regression

--- SDET/src/test/java/FileUtilityTest/LeadTest.java
+++ SDET/src/test/java/FileUtilityTest/LeadTest.java
@@ -5,10 +5,11 @@ import org.testng.annotations.Test;
 import com.crm.vtiger.objectRepository.HomePage;
 import com.crm.vtiger.objectRepository.PBaseClass;
 
-public class OrgTest extends PBaseClass{
+public class LeadTest extends PBaseClass {
 	@Test(groups={"SmokeTest","RegressionTest"})
-	public void createOrg() {
+	public void createLeads() throws Throwable {
 		HomePage hp = new HomePage(driver);
-		hp.getOrganizationLink().click();
+		hp.getLeadsLink().click();
 	}
+
 }
