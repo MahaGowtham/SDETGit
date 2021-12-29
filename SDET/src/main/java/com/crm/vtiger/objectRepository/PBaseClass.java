ad91516d6f3aa05e8d45ecff92f09992c860ed55
MahaGowtham <madhumeena.k1994@gmail.com>
2021-12-29 14:38:11 +0530

    Hi new commit

--- SDET/src/main/java/com/crm/vtiger/objectRepository/PBaseClass.java
+++ SDET/src/main/java/com/crm/vtiger/objectRepository/PBaseClass.java
@@ -32,7 +32,7 @@ public class PBaseClass extends WebDriverUtilities{
 		 */
 		@Parameters("browser")
 		
-		@BeforeClass
+		@BeforeClass(groups="smokeTest")
 		public void launchBrowser(String browser) throws Exception
 		{
 			//System.out.println("Launch the Browser");
@@ -49,7 +49,7 @@ public class PBaseClass extends WebDriverUtilities{
 			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		}
 		
-		@BeforeMethod
+		@BeforeMethod(groups="smokeTest")
 		public void createlogin() throws Exception {
 			LoginPage lp = new LoginPage(driver);
 			String user = pfile.readDataFromProperties("username");
@@ -57,14 +57,14 @@ public class PBaseClass extends WebDriverUtilities{
 			lp.loginApp(user, pass);
 		}
 		
-		@AfterMethod
+		@AfterMethod(groups="smokeTest")
 		public void logoutPage() {
 			LogoutPage lop = new LogoutPage(driver);
 			lop.administrator();
 			lop.logout();
 		}
 		
-		@AfterClass
+		@AfterClass(groups="smokeTest")
 		public void closeBrowser() {
 			statWD.close();
 		}
