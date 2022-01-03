916f368b66c7a05f20db8f4de486af1c74f0f559
MahaGowtham <madhumeena.k1994@gmail.com>
2022-01-03 15:23:31 +0530

    new

--- SDET/src/main/java/FileUtility/BaseClass1.java
+++ SDET/src/main/java/FileUtility/BaseClass1.java
@@ -2,6 +2,7 @@ package FileUtility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -15,27 +16,31 @@ public class BaseClass1 {
 	public Login in = new Login();
 	public Logout out = new Logout();
 	
-	@BeforeClass//(groups="smokeTest")
+	@BeforeClass
 	public void launchBrowser() throws Exception
 	{
-		System.out.println("Launch the Browser");
-		driver = new ChromeDriver(); 
+		//System.out.println("Launch the Browser");
+		String browser=pfile.readDataFromProperties("browser");
+		if(browser.equalsIgnoreCase("chrome"))
+			driver=new ChromeDriver();
+	else if(browser.equalsIgnoreCase("firefox"))
+		driver= new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get(pfile.readDataFromProperties("URL"));
 	}
 	
-	@BeforeMethod//(groups="smokeTest")
+	@BeforeMethod
 	public void createLogin() {
 		in.login(driver);
 	}
 	
-	@AfterMethod//(groups="smokeTest")
+	@AfterMethod
 	public void signOut()
 	{
 		out.logout(driver);
 	}
 	
-	@AfterClass//(groups="smokeTest")
+	@AfterClass
 	public void closeBrowser()
 	{
 		driver.close();
