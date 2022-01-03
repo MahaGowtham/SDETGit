7e9e480878ecaf4874dc450c4a52b67e003911ec
MahaGowtham <madhumeena.k1994@gmail.com>
2022-01-03 15:11:01 +0530

    new

--- SDET/src/main/java/DataDrivenTesting/ReadDataFromExcelFile.java
+++ SDET/src/main/java/DataDrivenTesting/ReadDataFromExcelFile.java
@@ -6,12 +6,13 @@ import java.io.IOException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+
 public class ReadDataFromExcelFile {
 
-	public static void main(String[] args) throws IOException    {
+	public static void main(String[] args) throws IOException     {
 
 		FileInputStream fis = new FileInputStream("./CommonDataExcelFile.xlsx");
-		Workbook wbf = WorkbookFactory.create(fis);
+	    Workbook wbf = WorkbookFactory.create(fis);
 		int i, j;
 		for(int i1=0; i1<=1; i1++) {
 			for(int j1=i1; j1<=10; j1++) {
